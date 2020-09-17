21f40382b9be8374058db21b1e256584dd81060f
Zagrit <WitoDelnat@users.noreply.github.com>
2020-09-17 15:16:08 +0200

    fix: use ready endpoint

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.get("/", (_, response) => {
   console.log("GET / 200");
   response.json({ endpoints: ["GET /health", "POST /hello", "GET /foo"] });
 });
-app.get("/health", (_, response) => {
+app.get("/ready", (_, response) => {
   console.log("GET /health 200");
   response.json({ status: "pass" });
 });
